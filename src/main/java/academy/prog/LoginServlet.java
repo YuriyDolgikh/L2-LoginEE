7391e9f32d3076732e136da20dbbd9646fcd180f
Dolgikh Yuriy <dolgih.yv@gmail.com>
2023-08-14 12:21:36 +0200

    Commit 14.08.2023 - Home Task is ready

--- src/main/java/academy/prog/LoginServlet.java
+++ src/main/java/academy/prog/LoginServlet.java
@@ -3,8 +3,6 @@ package academy.prog;
 import jakarta.servlet.http.*;
 import java.io.IOException;
 
-// Req -> (S -> S) -> jsp
-
 public class LoginServlet extends HttpServlet {
     static final String LOGIN = "admin";
     static final String PASS = "admin";
@@ -13,12 +11,21 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String login = request.getParameter("login");
         String password = request.getParameter("password");
-
-        if (LOGIN.equals(login) && PASS.equals(password)) {
+        String ageStr = request.getParameter("age");
+        int age = 0;
+        if(ageStr != null && !ageStr.equals("")){
+            age = Integer.parseInt(ageStr);
+        } else {
+            ageStr = "0";
+        }
+        if ( age < 18){
+            HttpSession session = request.getSession(true);
+            session.setAttribute("user_age", ageStr);
+        }else if (LOGIN.equals(login) && PASS.equals(password)) {
             HttpSession session = request.getSession(true);
             session.setAttribute("user_login", login);
+            session.setAttribute("user_age", ageStr);
         }
-
         response.sendRedirect("index.jsp");
     }
 
@@ -29,6 +36,7 @@ public class LoginServlet extends HttpServlet {
 
         if ("exit".equals(a) && (session != null))
             session.removeAttribute("user_login");
+            session.removeAttribute("user_age");
 
         response.sendRedirect("index.jsp");
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,12 +4,17 @@
     <title>Prog Academy</title>
   </head>
   <body>
-    <% String login = (String)session.getAttribute("user_login"); %>
+    <% String login = (String)session.getAttribute("user_login");
+       String age = (String)session.getAttribute("user_age"); %>
 
-    <% if (login == null || "".equals(login)) { %>
+    <% if (age != null && Integer.parseInt(age) < 18 && Integer.parseInt(age) > 0){ %>
+        <h1>Your age is < 18 !</h1>
+        <br>Try again <a href="/login?a=exit">Ok</a>
+    <% } else if (login == null || "".equals(login)) { %>
         <form action="/login" method="POST">
             Login: <input type="text" name="login"><br>
             Password: <input type="password" name="password"><br>
+            Age: <input type="text" name="age"><br>
             <input type="submit" />
         </form>
     <% } else { %>
